7b8b5d81fab199fb50a6fa952e10a4360f6e034a
Virul-git <katlavirul.vk@gmail.com>
2017-06-13 09:01:38 +0000

    tiles adding succesfull

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import pygame 
+from tileC import Tile
 
-
-display_width = 420
-display_height = 420
+display_width = 720
+display_height = 440
 	
 black = (0,0,0)
 red = (255,0,0)
@@ -11,9 +11,23 @@ blue =(0,0,255)
 
 pygame.init()
 
+############ tiles where the player doesn't step #############
+walls = (1,2,3,4,5,6,7,8,9,10)
 
 gameDisplay = pygame.display.set_mode((display_width,display_height))
-pygame.display.set_caption("BOX")
+pygame.display.set_caption("Tile Game")
+
+clock = pygame.time.Clock()
+FPS = 24
+total_frames = 0
+
+for y in range(0,display_height,40):
+	for x in range(0,display_width,40):
+		if Tile.total_tiles in walls:
+			Tile(x,y,'filled')
+		else:
+			Tile(x,y,'empty')
+
 
 
 def main():
@@ -27,6 +41,7 @@ def main():
 				if event.key == pygame.K_s:
 					pygame.quit()
 					quit()
+		Tile.draw_tiles(gameDisplay)
 		pygame.display.update()				
 	
 
--- tileC.py
+++ tileC.py
@@ -1,5 +1,9 @@
 import pygame
 
+black = (0,0,0)
+red = (255,0,0)
+green = (0,255,0)
+blue =(0,0,255)
 
 
 class Tile(pygame.Rect):
@@ -10,11 +14,23 @@ class Tile(pygame.Rect):
 
 	def __init__(self,x,y,Type):
 		self.type = Type
+		self.number = Tile.total_tiles
 		Tile.total_tiles += 1
 
 		if Type == 'empty':
-			self.walkable == True
+			self.walkable = True
 		else:
-			self.walkable == False
-		pygame.Rect(self,(x,y),(Tile.width,Tile.height))
+			self.walkable = False
+		pygame.Rect.__init__(self,(x,y),(Tile.width,Tile.height))
 		Tile.List.append(self)
+
+	@staticmethod
+	def get_tile(number):
+		for tile in Tile.List:
+			if tile.number == number:
+				return tile
+	@staticmethod
+	def draw_tiles(gameDisplay):
+		for tile in Tile.List:
+			if not (tile.type == 'empty'):
+				pygame.draw.rect(gameDisplay,blue,tile)
