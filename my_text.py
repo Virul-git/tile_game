3110053a138c0fefdaad82e004bb5effeb9cefde
Virul-git <katlavirul.vk@gmail.com>
2017-06-13 09:24:03 +0000

    text display file first step complete

---
+++ my_text.py
@@ -0,0 +1,14 @@
+import pygame
+
+
+def text_display(gameDisplay,text,x,y,size=12,color=(255,255,255),font='monospace'):
+
+		try:
+			text = str(text)
+			font = pygame.font.SysFont(font,size)
+			text = font.render(text,True,color)
+			gameDisplay.blit(text,(x,y))
+		except Exception, e:
+			print 'Error importing the given font.'
+			raise e
+		
