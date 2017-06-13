0ec88fa0d64fd039ed7de91502d116f783c0c137
Virul-git <katlavirul.vk@gmail.com>
2017-06-13 08:23:04 +0000

    tileC.py basic class complete

--- tileC.py
+++ tileC.py
@@ -2,7 +2,19 @@ import pygame
 
 
 
-class Tile(pygame.rect):
+class Tile(pygame.Rect):
 	
-	def __init__():
-		
+	List = []
+	width, height = 40,40
+	total_tiles = 1
+
+	def __init__(self,x,y,Type):
+		self.type = Type
+		Tile.total_tiles += 1
+
+		if Type == 'empty':
+			self.walkable == True
+		else:
+			self.walkable == False
+		pygame.Rect(self,(x,y),(Tile.width,Tile.height))
+		Tile.List.append(self)
